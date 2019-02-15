724fe8051e4095dd08d62d1c605f73bd75ffefdd
Angel Ortmann-Li <ali02@stuy.edu>
2019-02-15 10:15:49 -0500

    added comments

--- KnightBoard.java
+++ KnightBoard.java
@@ -13,6 +13,7 @@ public class KnightBoard{
       }
     }
     area = startingRows*startingCols;
+    //stores all the possible moves
     moves = new int[][]{{2,1}, {-2,-1},
                 {-2,1}, {2,-1},
                 {1,2}, {-1,-2},
@@ -44,11 +45,27 @@ public class KnightBoard{
     return solveHelp(startingRow, startingCol, 0);
   }
 
+  /*
+  the base case is if the nubmer move you're on is equal to the area of the board (meanuiing the number of all squares)
+  sHelp(r,c,n)
+    if num = area --> true
+    for (loop through possible moves)
+      set square = n
+      check if youre inside the KnightBoard
+      sHelp(r + move, c+ move, num++)
+    set equal to 0
+    false
+  */
+
   public boolean solveHelp(int r, int c, int num){
     if (num == area) return true;
+    //iterates through the possible moves
     for (int[] i : moves){
+        // sets the square value to ther number move you're on
         if(board[r][c] == 0) board[r][c] = num;
+        //checks if youre inside the board
         if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) return false;
+        //recursion that adds the possible moves
         return solveHelp(row + i[0], col + i[1], num + 1);
     }
     board[row + i[0]][col + i[1]] = 0;
@@ -73,7 +90,7 @@ public class KnightBoard{
     for (int[] i : moves){
       if(board[r][c] == 0) board[r][c] = num;
       if((row + i[0] >= 0 || row + i[0] < board.length || col + i[1] >= 0 || col + i[1] < board[0].length) && solve(r,c)){
-        out += cShelp(row + i[0], col + i[1], num + 1);
+        out += cShelp(row + i[0], col + i[1], num + 1ato);
       }
     }
     board[row + i[0]][col + i[1]] = 0;
