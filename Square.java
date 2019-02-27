c9470d178cb75f755b5ef983a89b1cfdadc47ed3
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-02-26 19:01:01 -0500

    set up Square object to help with the optimization

---
+++ Square.java
@@ -0,0 +1,33 @@
+public class Square implements Comparable<Tile>{
+  private int row;
+  private int col;
+  private int moves;
+  public Square(int row, int col, int n){
+    this.row = row;
+    this.col = col;
+    this.moves = n;
+  }
+
+  public String toString(){
+    return moves + "";
+  }
+
+  public int getRow(){
+    return row;
+  }
+  public int getCol(){
+    return col;
+  }
+  public int getMoves(){
+    return moves;
+  }
+
+  public void changeMoves(int n){
+    moves += n;
+  }
+
+  public int compareTo(Square yeet){
+    return moves - yeet.moves();
+  }
+
+}
